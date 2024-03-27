67cacd15604239ec5ae6cd50b377773d05806385
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-03-27 13:22:00 +0900

    :tada:QuizApp with HTML CSS AND JS:tada:

---
+++ script.js
@@ -0,0 +1,93 @@
+const quizData = [
+  {
+    question: "What is the most used programming language in 2019?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correctAnswer: "d"
+  },
+  {
+    question: "Who is the President of US?",
+    a: "Florin Pop",
+    b: "Donald Trump",
+    c: "Ivan Saldano",
+    d: "Mihai Andrei",
+    correctAnswer: "b"
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correctAnswer: "a"
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correctAnswer: "b"
+  }
+];
+
+const quiz = document.getElementById("quiz");
+const questionElement = document.getElementById('question');
+const answerElements = document.querySelectorAll('.answer');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+// initial call
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionElement.innerHTML = currentQuizData.question;
+  a_text.innerHTML = currentQuizData.a;
+  b_text.innerHTML = currentQuizData.b;
+  c_text.innerHTML = currentQuizData.c;
+  d_text.innerHTML = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+  answerElements.forEach((answerElement) => {
+    if (answerElement.checked) {
+      answer = answerElement.id;
+    }
+  });
+  return answer;
+}
+
+function deselectAnswers() {
+  answerElements.forEach((answerElement) => {
+    answerElement.checked = false;
+  });
+}
+
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correctAnswer) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+        <h1>You answered correctly at ${score}/${quizData.length} questions.</h1>
+        <button onclick="location.reload()">Reload</button>
+      `;
+    }
+  }
+});
